4d8ec3512a495e185b0e0127db81050ea1ba9ab7
1Eba <barkotka@gmail.com>
2021-11-04 23:18:09 +0000

    Simple shell

---
+++ exit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * sigintHandler - avoid ctrl + c
+ * @sig_num: value that received
+ * Return: nothing
+ */
+void sigintHandler(int sig_num __attribute__((unused)))
+{
+		write(1, "\nNew shell $ ", 13);
+}
